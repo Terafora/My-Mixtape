74087ba26ac5b6000da273e9b3c4aca20766c8eb
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-27 11:19:38 +0000

    Update static_root in settings

--- config/settings.py
+++ config/settings.py
@@ -186,7 +186,7 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),  # Example additional directory
