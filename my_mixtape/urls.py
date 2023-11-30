92896740e87e8a85f6c06f3ac41f60baac429776
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-30 13:37:19 +0000

    Update library to allow for creation of mix tapes. Create form for creating mix tapes

--- my_mixtape/urls.py
+++ my_mixtape/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Index, About, Library, OpenTrackList
+from .views import Index, About, Library, OpenTrackList, AddMixTape
 
 urlpatterns = [
     path('', Index.as_view(), name='home'),
     path('about/', About.as_view(), name='about'),
     path('library/', Library.as_view(), name='library'),
     path('mixtape/<int:mixtape_id>/', OpenTrackList.as_view(), name='mixtape_detail'),
+    path('add_mixtape/', AddMixTape.as_view(), name='add_mixtape'),
 ]
