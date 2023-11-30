220663ce4158ca0b6c919403ee3fe6bb41ec1460
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-30 17:45:44 +0000

    Update URLs with AddTrack view

--- my_mixtape/urls.py
+++ my_mixtape/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Index, About, Library, OpenTrackList, AddMixTape
+from .views import Index, About, Library, OpenTrackList, AddMixTape, AddTrack
 
 urlpatterns = [
     path('', Index.as_view(), name='home'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('library/', Library.as_view(), name='library'),
     path('mixtape/<int:mixtape_id>/', OpenTrackList.as_view(), name='mixtape_detail'),
     path('add_mixtape/', AddMixTape.as_view(), name='add_mixtape'),
+    path('<int:mixtape_id>/add_track/', AddTrack.as_view(), name='add_track'),
 ]
