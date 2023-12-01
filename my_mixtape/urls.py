3e3dbd8e4bb5bf2314296af4c1d5075c30a29fcd
Charlotte <charlie.stone649@gmail.com>
2023-12-01 12:36:19 +0000

    Update url with clearer paths and delete url

--- my_mixtape/urls.py
+++ my_mixtape/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import Index, About, Library, OpenTrackList, AddMixTape, AddTrack
+from .views import Index, About, Library, OpenTrackList, AddMixTape, AddTrack, DeleteMixtape
 
 urlpatterns = [
     path('', Index.as_view(), name='home'),
     path('about/', About.as_view(), name='about'),
     path('library/', Library.as_view(), name='library'),
-    path('mixtape/<int:mixtape_id>/', OpenTrackList.as_view(), name='mixtape_detail'),
     path('add_mixtape/', AddMixTape.as_view(), name='add_mixtape'),
+    path('mixtape/<int:mixtape_id>/', OpenTrackList.as_view(), name='mixtape_detail'),
+    path('mixtape/delete/<int:mixtape_id>/', DeleteMixtape.as_view(), name='delete_mixtape'),
     path('<int:mixtape_id>/add_track/', AddTrack.as_view(), name='add_track'),
 ]
