ce325333591a4b4479ac957e32338cf3fe88a6b5
Charlotte <charlie.stone649@gmail.com>
2023-12-03 18:14:18 +0000

    Create forms.py for addmixtapes

---
+++ my_mixtape/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Mixtape
+
+class MixtapeForm(forms.ModelForm):
+    class Meta:
+        model = Mixtape
+        fields = ['name', 'image', 'image_alt', 'about', 'genre']
