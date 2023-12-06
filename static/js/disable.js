d4ec49848bdbb2b58e2f16f70381859deb44b031
Charlotte <charlie.stone649@gmail.com>
2023-12-06 08:48:34 +0000

    Update JS script to correctly prevent  multiple creations

--- static/js/disable.js
+++ static/js/disable.js
@@ -1,13 +1,19 @@
+const addMixBtn = document.getElementsByClassName('addmixbtn')[0];
+let = counter = 0
+
 document.addEventListener('DOMContentLoaded', function() {
-    /**
-     * Disables add button add mixtape with a 2-second delay
-     */
-    const addMixBtn = document.getElementById('addmixbtn');
-    console.log(addMixBtn);
-    
-    addMixBtn.addEventListener('click', function() {
-        setTimeout(function() {
-            addMixBtn.disabled = true;
-        }, 5000); // 2000 milliseconds = 2 seconds
+
+
+
+/**
+* Disables add button on second press
+* For preventing duplicate creations
+*/
+addMixBtn.addEventListener('click', function() {
+    if(counter == 1){
+        addMixBtn.disabled = true;
+    } else {
+        counter++
+    }
     });
 });
