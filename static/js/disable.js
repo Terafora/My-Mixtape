e9bf5667a7d80eb6af563bd84e3da1604dcf7d44
Charlotte <charlie.stone649@gmail.com>
2023-12-06 12:22:35 +0000

    Input missing semi-colons

--- static/js/disable.js
+++ static/js/disable.js
@@ -1,9 +1,7 @@
 const addMixBtn = document.getElementsByClassName('addmixbtn')[0];
-let counter = 0
+let counter = 0;
 
 document.addEventListener('DOMContentLoaded', function() {
-
-
 
 /**
 * Disables add button on second press
@@ -13,7 +11,7 @@ addMixBtn.addEventListener('click', function() {
     if(counter == 1){
         addMixBtn.disabled = true;
     } else {
-        counter++
+        counter++;
     }
     });
 });
